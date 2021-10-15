c1e0749c17881985c593bd54c2b4524c36867202
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2021-10-15 23:52:52 +0500

    Adding Files

--- Scroll/script.js
+++ Scroll/script.js
@@ -38,3 +38,33 @@ window.addEventListener("scroll", function () {
     topLink.classList.remove("show-link");
   }
 });
+
+// Smooth Scroll:
+// 1. Get id:
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (item) {
+    // Prevent Default:
+    item.preventDefault();
+    // Navigate to Specific Spot:
+    const id = item.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    // Calculate the height:
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
