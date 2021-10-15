aaee1d2f809882465dcccf313d5893dfa9e84601
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2021-10-15 23:02:17 +0500

    Adding Files

---
+++ Scroll/script.js
@@ -0,0 +1,40 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position to the Viewport.
+// pageYOffset is a read-only window property that returns the number of pixels the document has been scrolled vertically.
+
+// Set Year in Footer:
+const year = document.getElementById("date");
+year.innerHTML = new Date().getFullYear();
+
+// Nav Toggle:
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  //   linksContainer.classList.toggle("show-links");
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
+// Fixed Navbar:
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
