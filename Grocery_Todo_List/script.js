b666e6a2e4b642c23fe36b18436c162b386b8120
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2021-10-19 21:51:58 +0500

    Adding Files

--- Grocery_Todo_List/script.js
+++ Grocery_Todo_List/script.js
@@ -17,6 +17,8 @@ let editID = "";
 form.addEventListener("submit", addItem);
 // Clear Items:
 clearBtn.addEventListener("click", clearItems);
+// Load Items:
+window.addEventListener("DOMContentLoaded", setupItems);
 
 // *** Functions ***
 // Add Item:
@@ -25,28 +27,7 @@ function addItem(event) {
     const value = grocery.value;
     const id = new Date().getTime().toString();
     if (value !== "" && editFlag === false) {
-        const element = document.createElement("article");
-        // add class:
-        element.classList.add("grocery-item");
-        // add id:
-        const attr = document.createAttribute("data-id");
-        attr.value = id;
-        element.setAttributeNode(attr);
-        element.innerHTML = `<p class="title">${value}</p>
-            <div class="btn-container">
-              <button type="button" class="edit-btn">
-                <i class="fas fa-edit"></i>
-              </button>
-              <button type="button" class="delete-btn">
-                <i class="fas fa-trash"></i>
-              </button>
-            </div>`;
-        const deleteBtn = element.querySelector(".delete-btn");
-        const editBtn = element.querySelector(".edit-btn");
-        deleteBtn.addEventListener("click", deleteItem);
-        editBtn.addEventListener("click", editItem);
-        // append child:
-        list.appendChild(element);
+        createListItem(id, value);
         // display alert:
         displayAlert("item added to the list", "success");
         // show item in list:
@@ -87,7 +68,7 @@ function clearItems() {
     container.classList.remove("show-container");
     displayAlert("list has been cleared", "success");
     setBackToDefault();
-    // localStorage.removeItem("list");
+    localStorage.removeItem("list");
 }
 
 // Delete Function:
@@ -131,9 +112,26 @@ function addToLocalStorage(id, value) {
     localStorage.setItem("list", JSON.stringify(items));
 }
 
-function removeFromLocalStorage(id) {}
+function removeFromLocalStorage(id) {
+    let items = getLocalStorage();
+    items = items.filter(function (item) {
+        if (item.id !== id) {
+            return item;
+        }
+    });
+    localStorage.setItem("list", JSON.stringify(items));
+}
 
-function editLocalStorage(id, value) {}
+function editLocalStorage(id, value) {
+    let items = getLocalStorage();
+    items = items.map(function (item) {
+        if (item.id === id) {
+            item.value = value;
+        }
+        return item;
+    });
+    localStorage.setItem("list", JSON.stringify(items));
+}
 
 function getLocalStorage() {
     return localStorage.getItem("list")
@@ -142,3 +140,37 @@ function getLocalStorage() {
 }
 
 // *** Setup Items ***
+function setupItems() {
+    let items = getLocalStorage();
+    if (items.length > 0) {
+        items.forEach(function (item) {
+            createListItem(item.id, item.value);
+        });
+        container.classList.add("show-container");
+    }
+}
+
+function createListItem(id, value) {
+    const element = document.createElement("article");
+    // add class:
+    element.classList.add("grocery-item");
+    // add id:
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+    const deleteBtn = element.querySelector(".delete-btn");
+    const editBtn = element.querySelector(".edit-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    editBtn.addEventListener("click", editItem);
+    // append child:
+    list.appendChild(element);
+}
